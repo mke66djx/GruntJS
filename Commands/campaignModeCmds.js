8ece85ea9c863f4e96965e7b81e362089717c5a9
Edit Beluli <ebelul2@gmail.com>
2017-02-23 18:03:06 -0800

    GruntJS: Removed some files & directories

--- Commands/campaignModeCmds.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(vorpal, options) {
-    // vorpal
-    //     .command('return', 'Returns to entry point')
-    //     .action(function(args, cb){
-    //         this.hide()
-    //         cb();
-    //     });
-}
