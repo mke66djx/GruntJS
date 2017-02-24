937e88d82f8f79027d67b7ab0265768dccad0e0c
Edit Beluli <ebelul2@gmail.com>
2017-02-23 18:05:41 -0800

    GruntJS: Added and reconfigured some command files

---
+++ commands/sellModeCmds.js
@@ -0,0 +1,10 @@
+module.exports = function(vorpal, options) {
+    // vorpal
+    //     .command('hello', 'says hello')
+    //     .action(function(args, cb){
+    //         this.hide()
+    //         cb();
+    //     });
+
+
+}
