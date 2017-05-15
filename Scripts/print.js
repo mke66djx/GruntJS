2ebeeb7b8c576080b190fad26bf9a5f0d7162330
Edit Beluli <ebelul2@gmail.com>
2017-05-15 14:49:12 -0700

    GruntJS: Added Scripts Directory

---
+++ Scripts/print.js
@@ -0,0 +1,33 @@
+var printfilename = 'output.pdf';
+var printer = require("printer")
+
+function printSingleFile(fileNamePath){
+
+    var filenamePrint = fileNamePath || __filename;
+
+    if( process.platform != 'win32') {
+        printer.printFile({filename:filenamePrint,
+            printer: process.env[3], // printer name, if missing then will print to default printer
+            success:function(jobID){
+                console.log("sent to printer with ID: "+jobID);
+            },
+            error:function(err){
+                console.log(err);
+            }
+        });
+    } else {
+        // not yet implemented, use printDirect and text
+        var fs = require('fs');
+        printer.printDirect({data:fs.readFileSync(filenamePrint),
+            printer: process.env[3], // printer name, if missing then will print to default printer
+            success:function(jobID){
+                console.log("sent to printer with ID: "+jobID);
+            },
+            error:function(err){
+                console.log(err);
+            }
+        });
+    }
+}
+
+module.exports.printSingleFile = printSingleFile;
---
+++ Scripts/toPdf.js
@@ -0,0 +1,15 @@
+var toPdf = require("office-to-pdf")
+
+//
+// function toPdf(filePath){
+//     var filename = fileNamePath || __filename;
+//
+//     //Convert docXfile to PDF
+//     toPdf("filename").then(
+//         () => {console.log("OK")},
+//         (err) => {console.log(err)}
+//     )
+//
+// }
+//
+// module.exports.toPdf = toPdf;
